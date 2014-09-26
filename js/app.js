49792aa56d44a61a419f4ff80640a5c88104c661
Julian Pineda <studio@miroamarillo.com>
2014-09-26 10:08:04 -0400

    Moving around the code.  Completing the provider and the config

--- js/app.js
+++ js/app.js
@@ -4,9 +4,54 @@
 * Simple Chrome Weather App
 */
 angular.module('myApp', [])
-	.controller('MainController', function($scope, $timeout){
+	.provider('Weather', function(){
+		var apiKey = "";
+
+		this.getUrl = function(type, ext) {
+		  return "http://api.wunderground.com/api/" +
+		    this.apiKey + "/" + type + "/q/" +
+		    ext + '.json';
+		};
+
+		this.setApiKey = function(key){
+			if(key) this.apiKey = key;
+		};
+
+		this.$get = function($q, $http){
+			var self = this;
+			return{
+				getWeatherForecast: function(city) {
+			      var d = $q.defer();
+			      $http({
+			        method: 'GET',
+			        url: self.getUrl("forecast", city),
+			        cache: true
+			      }).success(function(data) {
+			        // The wunderground API returns the
+			        // object that nests the forecasts inside
+			        // the forecast.simpleforecast key
+			        d.resolve(data.forecast.simpleforecast);
+			      }).error(function(err) {
+			        d.reject(err);
+			      });
+			      return d.promise;
+			    }
+			}
+		}
+	})
+	.config(function(WeatherProvider) {
+		WeatherProvider.setApiKey('edce8d7f316b6fe4');
+	})
+	.controller('MainController', function($scope, $timeout, Weather){
 		//Build the date object
 		$scope.date = {};
+
+	    $scope.weather = {}
+	    // Hardcode San_Francisco for now
+	    Weather.getWeatherForecast("CA/San_Francisco")
+	    .then(function(data) {
+	      $scope.weather.forecast = data;
+	    });
 
 		//Update function
 		var updateTime = function(){
@@ -16,17 +61,4 @@ angular.module('myApp', [])
 
 		//Kick off the update function
 		updateTime();
-	})
-	.provider('Weather', function(){
-		var apiKey = "edce8d7f316b6fe4";
-
-		this.setApiKey = function(key){
-			if(key) this.apiKey = key;
-		};
-
-		this.$get = function($http){
-			return{
-				// Service object
-			}
-		}
 	})
